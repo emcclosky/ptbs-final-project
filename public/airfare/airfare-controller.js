5071b81ba397980232bcc7c70b456c69fc167231
Elizabeth Arinder <emcclosky@gmail.com>
2018-03-20 18:40:31 -0400

    airfare api (#8)

--- public/airfare/airfare-controller.js
+++ public/airfare/airfare-controller.js
@@ -1,11 +1,26 @@
 (function(){
     angular
       .module('app')
-      .controller('AirfareController', function (APIFactory, DataFactory) {
+      .controller('AirfareController', function (APIFactoy, DataFactory, $http) {
       var vm = this;
-      var finalSelections = DataFactory.finalSelections;
-      
-      console.log('getUserResults', DataFactory.getUserResults());
+       var finalSelections = DataFactory.finalSelections;
+      $http.get("http://api.travelpayouts.com/v2/prices/month-matrix?show_to_affiliates=false&origin=DTT&destination=CHI&month=2018-03-17", { headers: {"X-Access-Token": "5c61b72f81da16dfda7b47729c783f90"}}).then(result => {
+        console.log(result);
+      })
+
 
     });
+//we added the below
+//$.getJSON not working
+    // $.getJSON('curl --include --header "X-Access-Token: 5c61b72f81da16dfda7b47729c783f90" "http://api.travelpayouts.com/v2/prices/month-matrix?currency=rub&origin=LED&destination=HKT&show_to_affiliates=true&month=2018-04-17"' + cityCode, function (data) {
+    //
+    //        var items = [],
+    //            $ul;
+    //
+    //        $.each(data, function (currency, val) {
+    //
+    //            items.push(currency + '<span class="price">' + val.flightPrice + '</span> <span class="city">' + val.principalcity + '</span>');
+    //        });
+
+
   })();
