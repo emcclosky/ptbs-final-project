a20f85a76f3f7d0f13caaf78d8ce3e690f6eb6cb
Elizabeth Arinder <emcclosky@gmail.com>
2018-03-22 19:30:23 -0400

    Returns prices for top 3 cities now

--- public/airfare/airfare-controller.js
+++ public/airfare/airfare-controller.js
@@ -1,13 +1,13 @@
 (function(){
   angular
     .module('app')
-    .controller('AirfareController', function (APIFactory, DataFactory, $http) {
+    .controller('AirfareController', function (APIFactory, $http, $timeout) {
     var vm = this;
-    vm.finalSelections = DataFactory.getUserResults();
-    vm.userAirport = DataFactory.getUserAirport();
-
-    APIFactory.apiCall(vm.userAirport, vm.finalSelections);  
-
+    vm.$onInit = function () {
+      APIFactory.apiCall().then(function(result){
+        $timeout(vm.finalSelections = result);
+      }); 
+    }
   });
 })();
 
--- public/factories/api-factory.js
+++ public/factories/api-factory.js
@@ -3,24 +3,34 @@
       .factory('APIFactory', function($http, DataFactory){
         var data;
 
-        function apiCall(userAirport, finalSelections){
-          airports = [];
-          for (var i = 0; i < 3; i++){
-            airports.push(finalSelections[i].airport);
-          };
-          for (var i = 0; i < 3; i ++){
-            var destination = airports[i];
-            console.log(destination);
+        function apiCall(){
+          var userAirport = DataFactory.getUserAirport();
+          var destinations = angular.copy(DataFactory.getUserResults()).slice(0, 3);
 
-            // removed both the from and to airport
-            
-            $http.get(`api/prices/month-matrix?currency=usd&show_to_affiliates=false&origin=${userAirport}&destination=${destination}&month=2018-04-21`).then(result => {
-              data = result;
-              console.log('it is me the data', data);
-            }); 
-          }}
+          var promises = [];
+          for (var i = 0; i < destinations.length; i ++){
+            var destination = destinations[i].airport;
+            promises.push($http.get(`api/prices/month-matrix?currency=usd&show_to_affiliates=false&origin=${userAirport}&destination=${destination}&month=2018-04-21`))
+          }
 
-      
+          return Promise.all(promises).then(function(result){
+            for (var i = 0; i < destinations.length; i++) {
+              var destination = destinations[i];
+              var price;
+              if (result[i].data) {
+                var prices = result[i].data.map(function(data){
+                  return data.value;
+                });
+                price = Math.min(...prices);
+              } else {
+                price = undefined;
+              }
+              Object.assign(destination, { price })
+            }
+            return destinations;
+          });
+        }
+
         
         return {
            data,
