bf1f9cb04e8b9b526e46e65db1e4cd7eb78987b8
Elizabeth Arinder <emcclosky@gmail.com>
2018-03-28 12:52:44 -0400

    Fixed a small piece of functionality in airfare controller, and made the airfare view mobile friendly.

--- public/factories/data-factory.js
+++ public/factories/data-factory.js
@@ -13,14 +13,14 @@
 
 
         var cities = [
-                {name: 'Bangkok', airport: 'BKK', tag: ['beach', 'crowded', 'adventure', 'party'], id: 1, activities:['Explore the Temple of the Emerald Buddha', 'Shop at the Floating Market', 'Tour the Summer Palace'], img: 'http://i63.tinypic.com/oiyi6h.jpg'},
-                {name: 'Reykjavik', airport: 'REK', tag: ['city', 'isolation', 'adventure', 'sleep'], id: 2, activities:['Visit Blue Lagoon', 'Take the Viking Horse-Riding Tour', 'Be enchanted by the Northern Lights'], img: 'http://i68.tinypic.com/2re1w5s.jpg'},
-                {name: 'Milan', airport: 'MIL', tag: ['city', 'crowded', 'lazy', 'sleep'], id: 3, activities:['Take in the ornate Milan Cathedral', 'Shop at the Galleria Vittorio Emanuele II', 'Tour the medieval castle Sforzesco' ], img: 'http://i65.tinypic.com/1675d9g.jpg'},
-                {name: 'New York City', airport: 'JFK', tag: ['city', 'crowded', 'adventure', 'party'], id: 4, activities:['Statue of Liberty and Ellis Island', 'Visit 9/11 Memorial', 'Go on the TCM Classic Film Tour of New York', 'Enjoy a show on Broadway', 'Take in the city from the Rockefeller Center Observation Deck'], img: 'http://i68.tinypic.com/2pq2rh5.jpg'},
-                {name: 'Havana', airport: 'EYW', tag: ['beach', 'crowded', 'lazy', 'sleep'], id: 5, activities:['Enjoy salsa dancing', 'Smoke a cuban cigar', 'Ride in a vintage convertible', 'Visit Ernest Hemingway\'s house'], img: ' http://i64.tinypic.com/2h3qv6v.jpg'},
-                {name: 'Dublin', airport: 'DUB', tag: ['city', 'isolation', 'lazy', 'sleep'], id: 6, activities:['Visit Blarney Castle', 'Take the Cliffs of Moher Tour', 'See the Guinness Storehouse', 'Irish Whiskey Museum Experience'], img: ' http://i67.tinypic.com/pytu8.jpg'},
-                {name: 'Mumbai', airport: 'BOM', tag: ['city', 'crowded', 'adventure', 'sleep'], id: 7, activities:['Take the Mumbai Cave Tour', 'Visit the Global Vipassana Pagoda', 'Go leisure sailing', 'See the Entrance to India'], img: 'http://i66.tinypic.com/2dir87t.jpg'},
-                {name: 'Cancun', airport: 'CUN', tag: ['beach', 'isolation', 'lazy', 'party'], id: 8, activities:['Explore the Tulum Ruins', 'Go reef and shipwreck snorkeling', 'Tour Chichen Itza and Cenote'], img: 'http://i65.tinypic.com/xu4na.jpg'}
+                {name: 'Bangkok', airport: 'BKK', tag: ['Beach', 'Crowded', 'Adventure', 'Party'], id: 1, activities:['Explore the Temple of the Emerald Buddha', 'Shop at the Floating Market', 'Tour the Summer Palace'], img: 'http://i63.tinypic.com/oiyi6h.jpg'},
+                {name: 'Reykjavik', airport: 'REK', tag: ['City', 'Isolation', 'Adventure', 'Sleep'], id: 2, activities:['Visit Blue Lagoon', 'Take the Viking Horse-Riding Tour', 'Be enchanted by the Northern Lights'], img: 'http://i68.tinypic.com/2re1w5s.jpg'},
+                {name: 'Milan', airport: 'MIL', tag: ['City', 'Crowded', 'Lazy', 'Sleep'], id: 3, activities:['Take in the ornate Milan Cathedral', 'Shop at the Galleria Vittorio Emanuele II', 'Tour the medieval castle Sforzesco' ], img: 'http://i65.tinypic.com/1675d9g.jpg'},
+                {name: 'New York City', airport: 'JFK', tag: ['City', 'Crowded', 'Adventure', 'Party'], id: 4, activities:['Statue of Liberty and Ellis Island', 'Visit 9/11 Memorial', 'Go on the TCM Classic Film Tour of New York', 'Enjoy a show on Broadway', 'Take in the city from the Rockefeller Center Observation Deck'], img: 'http://i68.tinypic.com/2pq2rh5.jpg'},
+                {name: 'Havana', airport: 'EYW', tag: ['beach', 'Crowded', 'Lazy', 'Sleep'], id: 5, activities:['Enjoy salsa dancing', 'Smoke a cuban cigar', 'Ride in a vintage convertible', 'Visit Ernest Hemingway\'s house'], img: ' http://i64.tinypic.com/2h3qv6v.jpg'},
+                {name: 'Dublin', airport: 'DUB', tag: ['City', 'Isolation', 'Lazy', 'Sleep'], id: 6, activities:['Visit Blarney Castle', 'Take the Cliffs of Moher Tour', 'See the Guinness Storehouse', 'Irish Whiskey Museum Experience'], img: ' http://i67.tinypic.com/pytu8.jpg'},
+                {name: 'Mumbai', airport: 'BOM', tag: ['City', 'Crowded', 'Adventure', 'Sleep'], id: 7, activities:['Take the Mumbai Cave Tour', 'Visit the Global Vipassana Pagoda', 'Go leisure sailing', 'See the Entrance to India'], img: 'http://i66.tinypic.com/2dir87t.jpg'},
+                {name: 'Cancun', airport: 'CUN', tag: ['Beach', 'Isolation', 'Lazy', 'Party'], id: 8, activities:['Explore the Tulum Ruins', 'Go reef and shipwreck snorkeling', 'Tour Chichen Itza and Cenote'], img: 'http://i65.tinypic.com/xu4na.jpg'}
                 ];
 
         var lists = [
@@ -35,6 +35,7 @@
           cities.forEach(function(city) {
             var match = intersection(city.tag, userChoices).length;
             finalSelections.push(Object.assign(city, { match }));
+            console.log('maybe work', finalSelections);
           });
 
           return finalSelections.sort(function(a, b) {
@@ -47,7 +48,6 @@
             lists,
             userChoices,
             getUserResults,
-            userChoices,
             setUserAirport,
             getUserAirport
         };
